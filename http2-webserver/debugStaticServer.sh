0c470dd17ec04d86f1bc648210bff93637a2ff01
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2021-05-02 21:49:43 -0400

    core_style.css -> .scss, project icons, and more... New/fixed scripts: buildcss.sh, debugStaticServer.sh Add project icons for poeatlas.net and fibermc.com (and corresponding css (hover effects!)) core_style.css is now made up of multiple component .scss partials. General improvements to site style (including, not limited):   - Header buttons   - dnd float msg box sizing on firefox   - Header box shadow less obtrusive   - Footer box shadow removed   - If project website link is not provided, website button is not generated, instead of generating a 'disabled' button. Swap out footer.js for a 1-line inline script (copyright notice auto-date generation) Add description text to mini-project images (not yet visible, is just in data) Add "menubar" role to navbar.

--- http2-webserver/debugStaticServer.sh
+++ http2-webserver/debugStaticServer.sh
@@ -1,2 +1,2 @@
-node ./http2server2.1.js -p 8084 --debug --pubpath ../public_static --maxAge 0 --trueDebug --log verbose
+nodemon --inspect --watch ../public_static --watch ./ ./http2server2.1.js -p 8084 --debug --pubpath ../public_static --maxAge 0 --log simple
 #node --inspect http2server2.0.js -p 8084 --debug --pubpath ../public_static --log simple -k ./credentials/server.key -c ./credentials/server.crt --maxAge 0
