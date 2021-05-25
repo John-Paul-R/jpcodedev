07d2a7e6fd8bc01e35129b0160df8073ddc3cc95
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2021-05-25 07:48:31 -0400

    Track copyright_text script.

---
+++ public_static/js/copyright_text.js
@@ -0,0 +1,4 @@
+
+(function(){
+    document.getElementById("copyright_text").innerHTML = `&#169; 2020-${new Date().getFullYear()} <a href="https://github.com/John-Paul-R"> John Paul R.</a>`;
+})();
