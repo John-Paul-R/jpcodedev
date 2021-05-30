55cb528c0a250e062a791642c1dfdebb23b773ff
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2021-05-30 17:05:52 -0400

    aria updates.   - fix up navbar roles (required rework of dropdown_self_contained)   - Add aria-labelledby for project item links.     (required adding id fields to index.pug.json)

--- public_static/js/dropdown_self_contained.js
+++ public_static/js/dropdown_self_contained.js
@@ -3,21 +3,19 @@ const wrappers = document.getElementsByClassName("dropdown_sc-wrapper");
 
 for (const wrapper of wrappers) {
     const content = wrapper.getElementsByClassName("dropdown_sc-content");
-    const buttons = wrapper.getElementsByClassName("dropdown_sc-button");
-    for (const button of buttons) {
-        button.addEventListener("click", (e)=>{
-            const content_elem = content[0]; 
-            const bodyListener = (e)=>{
-                wrapper.classList.remove('open');
-                document.body.removeEventListener('click', bodyListener);
-            };
-            
-            if (!wrapper.classList.contains('open')) {
-                // Going from closed to open
-                document.body.addEventListener("click", bodyListener);
-                e.stopPropagation();
-            }
-            wrapper.classList.toggle('open');
-        });
-    }
+    const button = document.getElementById(`${wrapper.id}-btn`);
+    button.addEventListener("click", (e)=>{
+        const content_elem = content[0]; 
+        const bodyListener = (e)=>{
+            wrapper.classList.remove('open');
+            document.body.removeEventListener('click', bodyListener);
+        };
+        
+        if (!wrapper.classList.contains('open')) {
+            // Going from closed to open
+            document.body.addEventListener("click", bodyListener);
+            e.stopPropagation();
+        }
+        wrapper.classList.toggle('open');
+    });
 }
