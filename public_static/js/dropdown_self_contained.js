dbba3125943db15b6be306086c7db7891d550668
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2024-10-05 15:46:36 -0400

    prevent clicks inside the popup from closing it... ... once again. (prevent `mousedown` propagation to `body` from w/in popup)

--- public_static/js/dropdown_self_contained.js
+++ public_static/js/dropdown_self_contained.js
@@ -2,20 +2,44 @@
 const wrappers = document.getElementsByClassName("dropdown_sc-wrapper");
 
 for (const wrapper of wrappers) {
-    const content = wrapper.getElementsByClassName("dropdown_sc-content");
+    const content = wrapper.getElementsByClassName("dropdown_sc-content")[0];
     const button = document.getElementById(`${wrapper.id}-btn`);
-    button.addEventListener("click", (e)=>{
-        const content_elem = content[0]; 
-        const bodyListener = (e)=>{
+
+    content.addEventListener("mousedown", (e) => {
+        // can't allow this event to reach `body` or the popup will close from
+        // clicks within it.
+        e.stopPropagation();
+    })
+    let isToggledDueToMouseDown;
+    let isClickFromClose = false;
+    const clickHandler = (e) => {
+        isClickFromClose = false;
+        const bodyListener = () => {
             wrapper.classList.remove('open');
-            document.body.removeEventListener('click', bodyListener);
+            document.body.removeEventListener('mousedown', bodyListener);
+            isToggledDueToMouseDown = undefined;
+            isClickFromClose = true;
         };
-        
+
         if (!wrapper.classList.contains('open')) {
-            // Going from closed to open
-            document.body.addEventListener("click", bodyListener);
             e.stopPropagation();
+            // Going from closed to open
+            document.body.addEventListener("mousedown", bodyListener);
         }
         wrapper.classList.toggle('open');
+    };
+
+    button.addEventListener("mousedown", (e) => {
+        isToggledDueToMouseDown = true;
+        clickHandler(e);
+    })
+    button.addEventListener("click", (e) => {
+        if (isToggledDueToMouseDown) {
+            // if "body"'s event fires, we'll close the menu we just
+            // opened, so prevent that
+            e.stopPropagation();
+        } else if (!isClickFromClose) {
+            clickHandler(e);
+        }
     });
 }
