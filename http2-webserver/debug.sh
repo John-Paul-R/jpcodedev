8635e6cbf4d4621651201f32dfc7a78ae1109f90
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2024-10-04 22:47:27 -0400

    disable inspect on static server by default

--- http2-webserver/debug.sh
+++ http2-webserver/debug.sh
@@ -11,7 +11,8 @@ run_www() {
 }
 
 run_static() {
-    nodemon -L --inspect -w ../public_static -w ./ -w ../pug ./ts_output/http2server2.1.js \
+    #--inspect 
+    nodemon -L -w ../public_static -w ./ -w ../pug ./ts_output/http2server2.1.js \
     -p $static --debug --pubpath ../public_static --maxAge 0 --log simple --host "static.jpcode.dev" \
     -k ./certs/localhost-privkey.pem \
     -c ./certs/localhost-cert.pem
