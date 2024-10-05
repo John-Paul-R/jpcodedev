5acc19f56581a690c2d9eed3542e2e2b98717a1b
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2024-10-05 13:55:27 -0400

    general resizable cleanup

--- public_static/js/resizable.js
+++ public_static/js/resizable.js
@@ -1,89 +1,84 @@
-(function() {
-var scriptTags = document.getElementsByTagName('script');
-var scriptTag = scriptTags[scriptTags.length - 1];
+(function () {
 
-var parentElem = scriptTag.parentElement;
-window.addEventListener('DOMContentLoaded', initResize);
-const STORAGE_KEY = 'preferred_reader_width';
+  window.addEventListener('DOMContentLoaded', initResize);
+  const STORAGE_KEY = 'preferred_reader_width';
+  const CSS_VAR_WIDTH = '--resizable-width';
 
-const originallyStoredSize = window.localStorage.getItem(STORAGE_KEY);
-if (originallyStoredSize !== null) {
-  window.document.documentElement.style.setProperty('--resizble-width', originallyStoredSize + 'px');
-}
+  const originallyStoredSize = window.localStorage.getItem(STORAGE_KEY);
+  if (originallyStoredSize !== null) {
+    window.document.documentElement.style.setProperty(CSS_VAR_WIDTH, originallyStoredSize + 'px');
+  }
 
-// Borrowed ideas from https://codepen.io/jkasun/pen/QrLjXP by Janith
+  // Borrowed ideas from https://codepen.io/jkasun/pen/QrLjXP by Janith
 
-function initResize() {
-    var startX, startWidth;
-    var elems = document.getElementsByClassName("resizable");
-    var element = null;
-    var storedWidth = window.localStorage.getItem(STORAGE_KEY)
+  function initResize() {
+    {
+      const elems = document.getElementsByClassName("resizable");
+      for (let i = 0; i < elems.length; i++) {
+        const el = elems[i];
 
-    for (var i = 0; i < elems.length; i++) {
-
-        var el = elems[i];
-        if (storedWidth)
-            el.style.setProperty('--resizble-width', `${storedWidth}px`);
-
-        var right = document.createElement("div");
+        const right = document.createElement("div");
         right.className = "resizer-right";
         el.appendChild(right);
         right.addEventListener("mousedown", initDrag, false);
         right.resizeParent = el;
-        right.addEventListener('dblclick', function (e) {
+        right.addEventListener('dblclick', () => {
           el.style.width = null;
           storeWidth(null);
         });
-        
+      }
+
     }
-    
+
+    let dragStartX, startWidth;
+    let draggingElementParent = null;
 
     function initDrag(e) {
-        element = this.resizeParent;
-    
-        startX = e.clientX;
-        startY = e.clientY;
-        startWidth = parseInt(
-          document.defaultView.getComputedStyle(element).width,
-          10
-        );
-        startHeight = parseInt(
-          document.defaultView.getComputedStyle(element).height,
-          10
-        );
-        document.documentElement.addEventListener("mousemove", doDrag, false);
-        document.documentElement.addEventListener("mouseup", stopDrag, false);
+      draggingElementParent = this.resizeParent;
+
+      dragStartX = e.clientX;
+      startY = e.clientY;
+      startWidth = parseInt(
+        document.defaultView.getComputedStyle(draggingElementParent).width,
+        10
+      );
+      startHeight = parseInt(
+        document.defaultView.getComputedStyle(draggingElementParent).height,
+        10
+      );
+      document.documentElement.addEventListener("mousemove", doDrag, false);
+      document.documentElement.addEventListener("mouseup", stopDrag, false);
     }
 
     function doDrag(e) {
-        let newWidth = startWidth + (e.clientX - startX)*2;
-        element.style.width = `clamp(50ch, ${newWidth}px, 100%)`;
-        storeWidth(newWidth);
+      let newWidth = startWidth + (e.clientX - dragStartX) * 2;
+      draggingElementParent.style.setProperty(CSS_VAR_WIDTH, `${newWidth}px`);
+      storeWidth(newWidth);
     }
-    
+
     function stopDrag() {
-        document.documentElement.removeEventListener("mousemove", doDrag, false);
-        document.documentElement.removeEventListener("mouseup", stopDrag, false);
+      document.documentElement.removeEventListener("mousemove", doDrag, false);
+      document.documentElement.removeEventListener("mouseup", stopDrag, false);
     }
 
     const debounce = (func, wait) => {
       let timeout;
-    
+
       return function executedFunction(...args) {
         const later = () => {
           clearTimeout(timeout);
           func(...args);
         };
-    
+
         clearTimeout(timeout);
         timeout = setTimeout(later, wait);
       };
     };
-    
+
     const storeWidth = debounce((width) => {
       window.localStorage.setItem(STORAGE_KEY, width);
     }, 500);
-      
-}
+
+  }
 
 })();
