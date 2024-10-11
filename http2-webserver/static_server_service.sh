06b753fa690874ea6eb6f4780e6607473731cfea
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2024-10-10 21:11:44 -0400

    grant sys uid permissions (seems deno fs wants, should be fine)

--- http2-webserver/static_server_service.sh
+++ http2-webserver/static_server_service.sh
@@ -6,7 +6,7 @@ deno run \
     --allow-read=".,../public,../public_static,../pug,src/foreign-api-cache" \
     --allow-write="../public,../public_static,log,src/foreign-api-cache" \
     --allow-env \
-    --allow-sys='homedir' \
+    --allow-sys='homedir,uid' \
     --allow-import='deno.land:443,jsr.io:443,esm.sh:443,raw.githubusercontent.com:443,user.githubusercontent.com:443,arweave.net:443,lra6z45nakk5lnu3yjchp7tftsdnwwikwr65ocha5eojfnlgu4sa.arweave.net:443' \
     --allow-net \
     ./src/http2server2.1.ts \
